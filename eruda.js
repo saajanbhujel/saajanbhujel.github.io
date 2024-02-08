61582cb94a10559c6a94fa106f0b3a723350b3c4
Saajan Bhujel <87616854+saajanbhujel@users.noreply.github.com>
2024-02-08 15:51:47 +0000

    test

--- eruda.js
+++ eruda.js
@@ -1,10 +1,6 @@
-/*
 ;(function () {
     var src = '//cdn.jsdelivr.net/npm/eruda';
     if (!/eruda=true/.test(window.location) && localStorage.getItem('active-eruda') != 'true') return;
     document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
     document.write('<scr' + 'ipt>eruda.init();</scr' + 'ipt>');
 })();
-*/
-
-(function () { var script = document.createElement('script'); script.src="https://cdn.jsdelivr.net/npm/eruda"; document.body.append(script); script.onload = function () { eruda.init(); } })();
