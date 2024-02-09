b040382d0a64fbd34b6c1ea3bec3b51a80c0c802
Saajan Bhujel <87616854+saajanbhujel@users.noreply.github.com>
2024-02-09 17:29:42 +0000

    Improved side menu

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-let menu = document.querySelector(".sideMenu");
+let sideMenu = document.querySelector(".sideMenu");
 let menuIcon = document.querySelector(".menuIcon");
 let closeIcon = document.querySelector(".closeIcon");
 
 function showMenu(){
     menuIcon.style.display = "none";
     closeIcon.style.display = "block";
-    menu.style.display = "flex";
+    sideMenu.style.display = "flex";
 }
 
 function closeMenu(){
-    menu.style.display = "none";
     menuIcon.style.display = "block";
     closeIcon.style.display = "none";
+    sideMenu.style.display = "none";
 }
