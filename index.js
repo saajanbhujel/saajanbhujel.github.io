282f87f8aff2fb909e83e93ed92468e3849b8f14
Saajan Bhujel <87616854+saajanbhujel@users.noreply.github.com>
2024-02-16 10:37:14 +0000

    improved code

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ let sideMenu = document.querySelector(".sideMenu");
 let menuIcon = document.querySelector(".menuIcon");
 let closeIcon = document.querySelector(".closeIcon");
 
-function showMenu(){
+function showMenu() {
     menuIcon.style.display = "none";
     closeIcon.style.display = "block";
     sideMenu.style.display = "flex";
 }
 
-function closeMenu(){
+function closeMenu() {
     menuIcon.style.display = "block";
     closeIcon.style.display = "none";
     sideMenu.style.display = "none";
